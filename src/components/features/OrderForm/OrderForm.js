62b761bb998f0f7f7a9cfa28f5f3d5b3394b57ab
Adrian Sadowski <sadow.adrian@gmail.com>
2021-10-20 18:49:23 +0200

    add prop cost OrderSummary

--- src/components/features/OrderForm/OrderForm.js
+++ src/components/features/OrderForm/OrderForm.js
@@ -2,17 +2,19 @@ import React from 'react';
 import { Col, Row } from 'react-flexbox-grid';
 //import styles from './OrderForm.scss';
 import OrderSummary from '../OrderSummary/OrderSummary';
+import PropTypes from 'prop-types';
 
-class OrderForm extends React.Component {
-  render() {
-    return (
-        <Row>
-          <Col xs={12}>
-            <OrderSummary />
-          </Col>
-        </Row>
-    )
-  }
+const OrderForm = (props) => (
+  <Row>
+    <Col xs={12}>
+      <OrderSummary cost={props.tripCost} />
+    </Col>
+  </Row>
+)
+
+
+OrderForm.propTypes = {
+  tripCost: PropTypes.string,
 }
 
 export default OrderForm
--- src/components/features/OrderSummary/OrderSummary.js
+++ src/components/features/OrderSummary/OrderSummary.js
@@ -1,16 +1,23 @@
 import React from 'react';
 import styles from './OrderSummary.module.scss';
-//import PropTypes from 'prop-types';
+import PropTypes from 'prop-types';
+// import { calculateTotal } from '../../../utils/calculateTotal';
+// import { formatPrice } from '../../../utils/formatPrice';
+// import pricing from '../data/pricing.json';
 
 
-class OrderSummary extends React.Component {
-  render () {
-    //const cost = this.props;
-    return (
-      <h2 className={styles.component}>"Total:"<strong>$123,450</strong></h2>
-    )
-  }
+
+const OrderSummary = ({cost}) =>(
+  <h2 className={styles.component}>Total:  
+    <strong>{cost}</strong>
+    {/* <strong>{formatPrice(calculateTotal(cost, ))}</strong> */}
+  </h2>
+
+)
+
+
+OrderSummary.propTypes = {
+  cost: PropTypes.string,
 }
-
 
 export default OrderSummary
