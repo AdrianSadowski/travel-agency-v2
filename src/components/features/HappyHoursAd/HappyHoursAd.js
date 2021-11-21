087ef2ef94dfdbd515c9c4a52ef752377a70feee
AdrianSadowski <sadow.adrian@gmail.com>
2021-11-21 13:03:58 +0100

    Add HappyHours test component

---
+++ src/components/features/HappyHoursAd/HappyHoursAd.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import styles from './HappyHoursAd.module.scss';
+
+const HappyHourAd = () => (
+  <div className={styles.component}>
+    
+  </div>
+);
+
+
+
+export default HappyHourAd;
---
+++ src/components/features/HappyHoursAd/HappyHoursAd.test.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import HappyHourAd from './HappyHoursAd';
+
+describe('Component HappyHoursAd', () => {
+  it('should reder without crashing', () => {
+    const component = shallow(<HappyHourAd />);
+    expect(component).toBeTruthy();
+  });
+});
