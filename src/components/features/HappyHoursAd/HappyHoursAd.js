009c23308f8914696009af1fc04e73e8d8ba8581
AdrianSadowski <sadow.adrian@gmail.com>
2021-11-22 19:35:19 +0100

    fix test 2

--- src/components/features/HappyHoursAd/HappyHoursAd.js
+++ src/components/features/HappyHoursAd/HappyHoursAd.js
@@ -4,7 +4,7 @@ import styles from './HappyHoursAd.module.scss';
 const HappyHourAd = () => (
   <div className={styles.component}>
     <h3 className={styles.title}>Happy Hour</h3>
-    <div className={styles.promoDescription}>time</div>
+    <div className={styles.promoDescription}>Time</div>
   </div>
 );
 
